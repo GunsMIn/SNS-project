446ec2e984791e32fee111369736b804c701e994
KimGunWoo9595 <medneter@naver.com>
2022-12-20 21:49:15 +0900

    ExceptionManager

---
+++ src/main/java/com/example/crudpersional/exceptionManager/ExceptionManager.java
@@ -0,0 +1,25 @@
+package com.example.crudpersional.exceptionManager;
+
+import com.example.crudpersional.domain.dto.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionManager {
+
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<?> userAppExceptionHandler(UserException e) {
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(Response.error(e.getErrorCode().getMessage()));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> runtimeExceptionHandler(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST) // 5xx에러를 400에러로 바꿔줌
+                .body(Response.error(e.getMessage()));
+    }
+
+}
