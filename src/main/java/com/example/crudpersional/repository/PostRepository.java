1141691bf4165af242f9a4e3c16e59d5197d676c
KimGunWoo9595 <medneter@naver.com>
2022-12-20 21:48:40 +0900

    Rep

--- src/main/java/com/example/crudpersional/repository/PostRepository.java
+++ src/main/java/com/example/crudpersional/repository/PostRepository.java
@@ -1,7 +1,11 @@
 package com.example.crudpersional.repository;
 
 import com.example.crudpersional.domain.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
+
+    Page<Post> findAll(Pageable pageable);
 }
