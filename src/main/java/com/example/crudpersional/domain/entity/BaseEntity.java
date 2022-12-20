d02871ab4a8f4c7f362bece5b3414b0e2ab1d40c
KimGunWoo9595 <medneter@naver.com>
2022-12-20 21:47:41 +0900

    BaseEntity

--- src/main/java/com/example/crudpersional/domain/entity/BaseEntity.java
+++ src/main/java/com/example/crudpersional/domain/entity/BaseEntity.java
@@ -1,14 +1,14 @@
 package com.example.crudpersional.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @MappedSuperclass
@@ -17,11 +17,32 @@ public abstract class BaseEntity {
 
     @Column(nullable = false, updatable = false)
     @CreatedDate
+    private String registeredAt;
+
+    @LastModifiedDate
+    private String updatedAt;
+
+    @PrePersist
+    public void onPrePersist(){
+        this.registeredAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+        this.updatedAt = this.registeredAt;
+    }
+
+    @PreUpdate
+    public void onPreUpdate(){
+        this.updatedAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
+    }
+ /*
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
     private LocalDateTime registeredAt;
 
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     @LastModifiedDate
-    private LocalDateTime updatedAt;
+    private LocalDateTime updatedAt;*/
 
     //https://thalals.tistory.com/302
     //삭제 시점
