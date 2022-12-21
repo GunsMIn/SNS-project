a09e456ed4ce43fbccfdfbe722cd505aeb92cb8b
KimGunWoo9595 <medneter@naver.com>
2022-12-21 11:04:04 +0900

    dto 어노테이션 추가

--- src/main/java/com/example/crudpersional/domain/dto/user/UserSelectResponse.java
+++ src/main/java/com/example/crudpersional/domain/dto/user/UserSelectResponse.java
@@ -2,6 +2,9 @@ package com.example.crudpersional.domain.dto.user;
 
 import com.example.crudpersional.domain.entity.Post;
 import com.example.crudpersional.domain.entity.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -10,6 +13,7 @@ import java.util.List;
 import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.EnumType.STRING;
 
+@AllArgsConstructor @NoArgsConstructor @Getter
 public class UserSelectResponse {
 
 
