cfacc1f6a73d7f69859ed78ae693d455137d91f9
KimGunWoo9595 <medneter@naver.com>
2022-12-21 22:05:43 +0900

    maincontroller

---
+++ src/main/java/com/example/crudpersional/domain/dto/user/UserDeleteRequest.java
@@ -0,0 +1,10 @@
+package com.example.crudpersional.domain.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor @NoArgsConstructor @Getter
+public class UserDeleteRequest {
+    Long userId;
+}
---
+++ src/main/java/com/example/crudpersional/mvc/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.example.crudpersional.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    //guns shop 메인화면
+    @RequestMapping("/main")
+    public String main() {
+        return "main";
+    }
+}
