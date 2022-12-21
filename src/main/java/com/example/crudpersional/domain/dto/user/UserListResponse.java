267f60b82fb3bfa8fbff2cb85345e2a96fc704f7
KimGunWoo9595 <medneter@naver.com>
2022-12-21 11:43:59 +0900

    회원 전체 조회 dto

---
+++ src/main/java/com/example/crudpersional/domain/dto/user/UserListResponse.java
@@ -0,0 +1,12 @@
+package com.example.crudpersional.domain.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor @Getter
+public class UserListResponse {
+
+    private Long id;
+
+    private String userName;
+}
