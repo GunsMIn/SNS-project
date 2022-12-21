22db0e712e57bcaf3e3fe23ba6d9449b3076ec27
KimGunWoo9595 <medneter@naver.com>
2022-12-21 13:14:26 +0900

    List 응답 제네릭 타입 객체

--- src/main/java/com/example/crudpersional/domain/dto/Result.java
+++ src/main/java/com/example/crudpersional/domain/dto/Result.java
@@ -1,4 +1,12 @@
 package com.example.crudpersional.domain.dto;
 
-public class Result {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Result<T> { // count 필요 응답 class
+    private int count;
+    private T data;
+
 }
