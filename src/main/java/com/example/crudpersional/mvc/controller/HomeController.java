201e7f6875fc46ffcbdd4b1fa5e9df4ab28e5c9b
KimGunWoo9595 <medneter@naver.com>
2022-12-22 14:31:28 +0900

    Homecontroller

---
+++ src/main/java/com/example/crudpersional/mvc/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.crudpersional.mvc.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    @RequestMapping("/")
+    public String goHome() {
+        return "index";
+    }
+
+}
