cd2738b13fb6f081070ae1831ade272de5670021
KimGunWoo9595 <medneter@naver.com>
2022-12-24 14:52:21 +0900

    validation 추가

--- src/main/java/com/example/crudpersional/mvc/dto/PostForm.java
+++ src/main/java/com/example/crudpersional/mvc/dto/PostForm.java
@@ -1,14 +1,31 @@
 package com.example.crudpersional.mvc.dto;
 
+import com.example.crudpersional.domain.entity.Post;
+import com.example.crudpersional.domain.entity.User;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data @AllArgsConstructor @NoArgsConstructor
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostForm {
 
-    //userId는 추후 다르게 넣어 줄것이다.
     private String userName;
+    @NotEmpty(message ="글의 제목을 입력해주세요")
     private String title;
+    @NotEmpty(message ="글의 내용을 입력해주세요")
     private String body;
+
+    //User user
+    public Post toEntity(User user) {
+        return Post.builder()
+                .title(title)
+                .body(body)
+                .user(user)
+                .build();
+    }
 }
