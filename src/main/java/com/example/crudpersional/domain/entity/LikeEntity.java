d008c168625405dd979e710ef1e57ec6e07c14c7
KimGunWoo9595 <medneter@naver.com>
2022-12-26 15:52:47 +0900

    Like Entity (좋아요)

---
+++ src/main/java/com/example/crudpersional/domain/entity/LikeEntity.java
@@ -0,0 +1,39 @@
+package com.example.crudpersional.domain.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class LikeEntity extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "like_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    private Integer count;
+
+    //연관관계 편의 메서드
+    //1. user
+    //2. post의 값을 like 엔티티에 넣어준다.
+    public static LikeEntity of(User user, Post post) {
+        LikeEntity like = new LikeEntity();
+        like.setUser(user);
+        like.setPost(post);
+        return like;
+    }
+}
