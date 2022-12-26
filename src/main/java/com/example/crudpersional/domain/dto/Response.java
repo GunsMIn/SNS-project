967807c86245d831d24f689051191ef71417f6ba
KimGunWoo9595 <medneter@naver.com>
2022-12-26 15:53:23 +0900

    Response 반환 메서드 투가

--- src/main/java/com/example/crudpersional/domain/dto/Response.java
+++ src/main/java/com/example/crudpersional/domain/dto/Response.java
@@ -17,9 +17,19 @@ public class Response<T> {
     public static <T> Response<T> success(T result) {
         return new Response("SUCCESS", result);
     }
+
     public static Response<Void> success() {
         return new Response("SUCCESS", null);
     }
+
+    public static Response<Void> success(String message) {
+        return new Response("SUCCESS", message);
+    }
+
+    public static Response<String> successToMessage (String message) {
+        return new Response("SUCCESS", message);
+    }
+
 
 
 
