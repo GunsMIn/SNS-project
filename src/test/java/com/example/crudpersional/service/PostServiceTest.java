c982cf3b7539573e24a8172b84968f21d0c16f17
KimGunWoo9595 <medneter@naver.com>
2022-12-27 13:56:42 +0900

    PostService Test setting

--- src/test/java/com/example/crudpersional/service/PostServiceTest.java
+++ src/test/java/com/example/crudpersional/service/PostServiceTest.java
@@ -1,17 +1,37 @@
 package com.example.crudpersional.service;
 
+import com.example.crudpersional.domain.dto.post.PostAddRequest;
+import com.example.crudpersional.domain.dto.post.PostAddResponse;
 import com.example.crudpersional.domain.dto.post.PostSelectResponse;
+import com.example.crudpersional.domain.dto.post.PostUpdateRequest;
 import com.example.crudpersional.domain.entity.Post;
 import com.example.crudpersional.domain.entity.User;
+import com.example.crudpersional.domain.entity.UserRole;
+import com.example.crudpersional.exceptionManager.UserException;
+import com.example.crudpersional.repository.LikeRepository;
 import com.example.crudpersional.repository.PostRepository;
 import com.example.crudpersional.repository.UserRepository;
 import lombok.Data;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -32,15 +52,18 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PostServiceTest {
+
+
     PostService postService;
 
     PostRepository postRepository = Mockito.mock(PostRepository.class);
     UserRepository userRepository = Mockito.mock(UserRepository.class);
+    LikeRepository likeRepository = Mockito.mock(LikeRepository.class);
 
 
     @BeforeEach
     void setUp() {
-        postService = new PostService(postRepository, userRepository);
+        postService = new PostService(postRepository, userRepository,likeRepository);
     }
 
     @Data
@@ -54,8 +77,8 @@ class PostServiceTest {
 
         public PostAndUser getDto() {
             PostAndUser p = new PostAndUser();
-            p.setPostId(100L);
-            p.setUserId(100L);
+            p.setPostId(1L);
+            p.setUserId(1L);
             p.setUserName("KIMGUNWOO");
             p.setPassword("1234");
             p.setTitle("테스트 제목");
@@ -64,14 +87,41 @@ class PostServiceTest {
         }
     }
 
+    @Data
+    public static class PostTestEntity{
+        public static Post get(String userName, String password) {
+            Post postEntity = Post.builder()
+                    .id(1L)
+                    .user(UserTestEntity.get(userName, password))
+                    .title("테스트 제목")
+                    .body("테스트 내용")
+                    .build();
+            return postEntity;
+        }
+    }
+
+    @Data
+    public static class UserTestEntity {
+        public static User get(String userName, String password) {
+            User entity = new User();
+            entity.setId(1L);
+            entity.setUserName(userName);
+            entity.setPassword(password);
+            entity.setRole(UserRole.USER);
+            return entity;
+        }
+    }
+
+
     @Test
     @DisplayName("조회 성공")
-    void success_post_get() {
+    void 글_조회_단건() {
         PostAndUser postAndUser = new PostAndUser();
         PostAndUser dto = postAndUser.getDto();
 
         User userEntity = new User();
         userEntity.setUserName(dto.getUserName());
+        userEntity.setPassword(dto.getPassword());
 
         Post postEntity = Post.builder()
                 .id(100L)
@@ -86,5 +136,26 @@ class PostServiceTest {
         PostSelectResponse postSelectResponse = postService.getPost(dto.getPostId());
         assertEquals(dto.getUserName(), postSelectResponse.getUserName());
     }
+
+    @Test
+    @DisplayName("글 등록 성공 테스트")
+    void post_success() {
+        PostAndUser postAndUser = new PostAndUser();
+        PostAndUser dto = postAndUser.getDto();
+
+        //목 객체 생성
+        Post mockPost = mock(Post.class);
+        User mockUser = mock(User.class);
+
+        when(userRepository.findByUserName(fixture.getUserName()))
+                .thenReturn(Optional.of(mockUserEntity));
+        when(postRepository.save(any()))
+                .thenReturn(mockPostEntity);
+
+        Assertions.assertDoesNotThrow(() -> postService.write(fixture.getTitle(), fixture.getBody(), fixture.getUserName()));
+    }
+
+
+
 
 }
