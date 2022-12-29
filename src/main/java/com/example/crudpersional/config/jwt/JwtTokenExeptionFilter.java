fa2155b7636135047e915940aae24f9f5b9d263e
KimGunWoo9595 <medneter@naver.com>
2022-12-29 19:10:08 +0900

    jwtTokenFilters 추가 (예외 처리)

---
+++ src/main/java/com/example/crudpersional/config/jwt/JwtTokenExeptionFilter.java
@@ -0,0 +1,67 @@
+package com.example.crudpersional.config.jwt;
+
+
+import com.example.crudpersional.domain.dto.Response;
+import com.example.crudpersional.exceptionManager.ErrorCode;
+import com.example.crudpersional.exceptionManager.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+class JwtTokenExceptionFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+//            log.info("토큰 검증 시작");
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException e) {       // 유효기간 만료 토큰
+            log.error("만료된 JWT 토큰입니다.");
+            setErrorResponse(response, ErrorCode.INVALID_TOKEN);
+        } catch (MalformedJwtException  e) {     // 구성이 잘못된 토큰(헤더,내용,서명이 없는 경우)
+            log.error("올바르게 구성되지 못한 JWT 토큰입니다.");
+            setErrorResponse(response, ErrorCode.INVALID_TOKEN);
+        } catch (SignatureException e) {        // 서명을 확인할 수 없는 토큰
+            log.error("서명을 확인할 수 없는 토큰입니다.");
+            setErrorResponse(response, ErrorCode.INVALID_TOKEN);
+        } catch (UnsupportedJwtException e) {   // 형식이 이상한 토큰
+            log.error("지원하지 않는 형식의 JWT 토큰입니다.");
+            setErrorResponse(response, ErrorCode.INVALID_TOKEN);
+        } catch (IllegalArgumentException | JwtException e) {  // 잘못된 토큰
+            log.error("잘못된 JWT 토큰입니다.");
+            setErrorResponse(response, ErrorCode.INVALID_TOKEN);
+
+        }
+    }
+
+    private void setErrorResponse(HttpServletResponse response, ErrorCode errorCode) throws IOException {
+
+        response.setStatus(errorCode.getStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+//        Gson gson = new Gson();
+
+        ErrorResponse errorResponse = new ErrorResponse(errorCode);
+        response.getWriter().write(objectMapper.writeValueAsString(Response.error("ERROR",errorResponse)));
+//        response.getWriter().write(gson.toJson(Response.error("ERROR",errorResponse)));
+
+    }
+
+
+}
