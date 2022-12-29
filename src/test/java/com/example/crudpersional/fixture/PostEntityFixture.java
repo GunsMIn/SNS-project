aa6c222f89787fbb8e170bdeffd06394c09d6ede
KimGunWoo9595 <medneter@naver.com>
2022-12-29 13:12:43 +0900

    ref : 타임리프 오류 수정

---
+++ src/test/java/com/example/crudpersional/fixture/PostEntityFixture.java
@@ -0,0 +1,15 @@
+package com.example.crudpersional.fixture;
+
+import com.example.crudpersional.domain.entity.Post;
+
+public class PostEntityFixture {
+    public static Post get(String userName, String password) {
+        Post post = Post.builder()
+                .id(1L)
+                .user(UserEntityFixture.get(userName, password))
+                .title("title")
+                .body("body")
+                .build();
+        return post;
+    }
+}
