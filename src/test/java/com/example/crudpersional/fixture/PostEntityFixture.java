5617ffb6497a704bb414be4600f2179339aebe6f
KimGunWoo9595 <medneter@naver.com>
2022-12-31 14:48:04 +0900

    PostService Test(수정 성공/수정 실패) TDD

--- src/test/java/com/example/crudpersional/fixture/PostEntityFixture.java
+++ src/test/java/com/example/crudpersional/fixture/PostEntityFixture.java
@@ -1,12 +1,24 @@
 package com.example.crudpersional.fixture;
 
 import com.example.crudpersional.domain.entity.Post;
+import com.example.crudpersional.domain.entity.User;
+
 //post
 public class PostEntityFixture {
     public static Post get(String userName, String password) {
         Post post = Post.builder()
                 .id(1L)
                 .user(UserEntityFixture.get(userName, password))
+                .title("title")
+                .body("body")
+                .build();
+        return post;
+    }
+
+    public static Post get(User user) {
+        Post post = Post.builder()
+                .id(1L)
+                .user(user)
                 .title("title")
                 .body("body")
                 .build();
--- src/test/java/com/example/crudpersional/service/PostServiceTest.java
+++ src/test/java/com/example/crudpersional/service/PostServiceTest.java
@@ -1,21 +1,22 @@
 package com.example.crudpersional.service;
-import com.example.crudpersional.domain.dto.post.PostAddRequest;
-import com.example.crudpersional.domain.dto.post.PostAddResponse;
-import com.example.crudpersional.domain.dto.post.PostSelectResponse;
-import com.example.crudpersional.domain.dto.post.PostUpdateRequest;
+import com.example.crudpersional.domain.dto.post.*;
 import com.example.crudpersional.domain.entity.Post;
 import com.example.crudpersional.domain.entity.User;
 import com.example.crudpersional.domain.entity.UserRole;
 import com.example.crudpersional.exceptionManager.ErrorCode;
 import com.example.crudpersional.exceptionManager.PostException;
 import com.example.crudpersional.exceptionManager.UserException;
+import com.example.crudpersional.fixture.PostAndUser;
+import com.example.crudpersional.fixture.PostEntityFixture;
+import com.example.crudpersional.fixture.UserEntityFixture;
 import com.example.crudpersional.repository.LikeRepository;
 import com.example.crudpersional.repository.PostRepository;
 import com.example.crudpersional.repository.UserRepository;
 import lombok.Data;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
@@ -23,161 +24,189 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-import org.junit.jupiter.api.Assertions;
-import org.springframework.security.test.context.support.WithMockUser;
 
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.expression.spel.ast.OpInc;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.util.Assert;
+
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class PostServiceTest {
 
 
+    @InjectMocks
     PostService postService;
 
-    PostRepository postRepository = Mockito.mock(PostRepository.class);
-    UserRepository userRepository = Mockito.mock(UserRepository.class);
-    LikeRepository likeRepository = Mockito.mock(LikeRepository.class);
-
-
-    @BeforeEach
-    void setUp() {
-        postService = new PostService(postRepository, userRepository,likeRepository);
-    }
-
-    @Data
-    public static class PostAndUser{
-        private Long postId;
-        private Long userId;
-        private String userName;
-        private String password;
-        private String title;
-        private String body;
-
-        public PostAndUser getDto() {
-            PostAndUser p = new PostAndUser();
-            p.setPostId(1L);
-            p.setUserId(1L);
-            p.setUserName("test");
-            p.setPassword("1234");
-            p.setTitle("테스트 제목");
-            p.setBody("테스트 내용");
-            return p;
-        }
-    }
-
-    @Data
-    public static class PostTestEntity{
-        public static Post get(String userName, String password) {
-            Post postEntity = Post.builder()
-                    .id(1L)
-                    .user(UserTestEntity.get(userName, password))
-                    .title("테스트 제목")
-                    .body("테스트 내용")
-                    .build();
-            return postEntity;
-        }
-    }
-////
-    @Data
-    public static class UserTestEntity {
-        public static User get(String userName, String password) {
-            User entity = new User();
-         /*   entity.setId(1L);
-            entity.setUserName(userName);
-            entity.setPassword(password);
-            entity.setRole(UserRole.USER);*/
-            return entity;
-        }
-    }
-
+    @Mock
+    PostRepository postRepository ;
+    @Mock
+    UserRepository userRepository ;
+    @Mock
+    LikeRepository likeRepository;
 
     @Test
     @DisplayName("조회 성공")
     void 글_조회_단건() {
+        //postAndUser의 객체
         PostAndUser postAndUser = new PostAndUser();
         PostAndUser dto = postAndUser.getDto();
 
-        User userEntity = new User();
-      /*  userEntity.setUserName(dto.getUserName());
-        userEntity.setPassword(dto.getPassword());*/
+        //User 픽스처로 생성된 user엔티티(매개변수는 postAndUser픽스처의 값)
+        User user = UserEntityFixture.get(dto.getUserName(), dto.getPassword());
+        //Post 픽스처로 생성된 user엔티티(매개변수는 User픽스처의 값)
+        Post post = PostEntityFixture.get(user);
 
-        Post postEntity = Post.builder()
-                .id(100L)
-                .user(userEntity)
-                .title("테스트 제목")
-                .body("테스트 내용")
-                .build();
-
-
-        when(postRepository.findById(dto.getPostId())).thenReturn(Optional.of(postEntity));
-
+        //postRepository에서 조회 메서드를 사용하면 post엔티티를 반환한다는 가정
+        when(postRepository.findById(dto.getPostId())).thenReturn(Optional.of(post));
+        //postService에서 포스트 단건조회
         PostSelectResponse postSelectResponse = postService.getPost(dto.getPostId());
+        //픽스처의 값과 service 반환 값의 테스트
         assertEquals(dto.getUserName(), postSelectResponse.getUserName());
     }
 
-    @Test
-    @DisplayName("글 등록 성공 테스트")
-    void 등록성공() {
-        PostAndUser postAndUser = new PostAndUser();
-        PostAndUser postAndUserDto = postAndUser.getDto();
+    @Nested
+    @DisplayName("포스트 등록")
+    class PostAdd {
+        @Test
+        @DisplayName("글 등록 성공 테스트")
+        void 등록성공() {
 
-        //목 객체 생성
-        Post mockPost = mock(Post.class);
-        User mockUser = mock(User.class);
+            PostAndUser dto = PostAndUser.getDto();
+            //User 픽스처로 생성된 user엔티티(매개변수는 postAndUser픽스처의 값)
+            User user = UserEntityFixture.get(dto.getUserName(), dto.getPassword());
+            //Post 픽스처로 생성된 user엔티티(매개변수는 User픽스처의 값)
+            Post post = PostEntityFixture.get(user);
+            //postservice의 작성에서 우선 회원을 찾을 것이다. userRepository.findOptionalByUserName
+            when(userRepository.findOptionalByUserName(any())).thenReturn(Optional.of(user));
+            //그 후 postRepository.save()를 적용
+            when(postRepository.save(any())).thenReturn(post);
+            //postService에 들어갈 변수
+            PostAddRequest postAddRequest = new PostAddRequest(dto.getTitle(), dto.getBody());
 
-       // when(userRepository.save(user)).thenReturn(Optional.of(User));
-        //user 확인
-        when(userRepository.findOptionalByUserName(any()))
-                .thenReturn(Optional.of(mockUser));
-       //post save 확인
-        when(postRepository.save(any()))
-                .thenReturn(mockPost);
+            PostAddResponse postAddResponse = postService.addPost(postAddRequest, dto.getUserName());
 
-        PostAddRequest postAddRequest = new PostAddRequest(postAndUser.getTitle(), postAndUser.getBody());
+            Assertions.assertEquals(postAddResponse.getPostId(), dto.getPostId());
+            //assertDoesNotThrow() 이상이 없다면 통과
+            Assertions.assertDoesNotThrow(() -> postAddResponse);
+        }
 
-        PostAddResponse postAddResponse = postService.addPost(postAddRequest, postAndUser.getUserName());
-        //Assertions.assertEquals(postAddResponse.getPostId(),0l);
-        Assertions.assertDoesNotThrow(() -> postService.addPost(postAddRequest, postAndUser.getUserName()));
+
+        //포스트생성시 유저가 존재하지 않을 때 에러
+        @Test
+        @WithMockUser
+        @DisplayName("등록 실패 : 유저 존재 하지 않음")
+        void 등록_실패() {
+            PostAndUser dto = PostAndUser.getDto();
+            User user = UserEntityFixture.get(dto.getUserName(), dto.getPassword());
+            Post post = PostEntityFixture.get(user);
+            // 유저 존재 하지 않은 상황 가정
+            when(userRepository.findOptionalByUserName(any())).thenReturn(Optional.empty());
+            when(postRepository.save(any())).thenReturn(mock(Post.class));
+
+            PostAddRequest postAddRequest = new PostAddRequest(dto.getTitle(), dto.getBody());
+
+            UserException userException
+                    = assertThrows(UserException.class, () -> postService.addPost(postAddRequest, dto.getUserName()));
+
+            Assertions.assertEquals(userException.getErrorCode(), ErrorCode.USERNAME_NOT_FOUND);
+            Assertions.assertEquals(userException.getMessage(),"회원가입 후 작성해주세요");
+        }
     }
 
-
-    //포스트생성시 유저가 존재하지 않을 때 에러
-    @Test
-    @WithMockUser
-    @DisplayName("등록 실패 : 유저 존재하지 않음")
-    void 등록실패() {
-        PostAndUser postAndUser = new PostAndUser();
-        PostAndUser postAndUserDto = postAndUser.getDto();
-
-        PostAddRequest postAddRequest = new PostAddRequest(postAndUser.getTitle(), postAndUser.getBody());
-
-
-        when(userRepository.findOptionalByUserName(postAndUserDto.getUserName())).thenReturn(Optional.empty());
-        when(postRepository.save(any())).thenReturn(mock(Post.class));
-
-        UserException exception = Assertions.assertThrows(UserException.class, () -> postService.addPost(postAddRequest, postAndUser.getUserName()));
-        assertEquals(ErrorCode.USERNAME_NOT_FOUND, exception.getErrorCode());
-    }
+    @Nested
+    @DisplayName("포스트 수정")
+    class PostUpdate{
 
     @Test
     @DisplayName("수정 실패 : 작성자!=유저")
-    void 수정_실패() {
-        PostAndUser postAndUser = new PostAndUser();
-        PostAndUser postAndUserDto = postAndUser.getDto();
-        User user1 = UserTestEntity.get("user", "password");
-        User user2 = UserTestEntity.get("user2", "password2");
+    void 수정_실패_작성자_불일치() {
 
-        //when(postRepository.findById(fixture.getPostId())).thenReturn(Optional.of(mockPostEntity));
-        when(postRepository.findById(postAndUserDto.getPostId())).thenReturn(Optional.of(Post.of("title","body",user1)));
-        //when(userRepository.findOptionalByUserName(user1.getUserName())).thenReturn(Optional.of(User.of(user2.getUserName(), user2.getPassword())));
+        PostAndUser dto = PostAndUser.getDto();
+        User user1 = UserEntityFixture.get("박지성", "password");
+        User user2 = UserEntityFixture.get("손흥민", "password2");
+        //user2가 작성한 post
+        Post postByUser2 = PostEntityFixture.get(user2);
+        //수정 request
+        PostUpdateRequest postUpdateRequest = new PostUpdateRequest("수정될 제목", "수정될 내용");
+        // post를 작성한 user는 user1
+        when(postRepository.findById(dto.getPostId()))
+                .thenReturn(Optional.of(Post.of("title","body",user1)));
+        // post를 수정 시도한 user는 user2
+        when(userRepository.findOptionalByUserName(any()))
+                .thenReturn(Optional.of(User.of(user2.getUsername(), user2.getPassword())));
 
-        PostUpdateRequest postUpdateRequest = new PostUpdateRequest(postAndUserDto.getTitle(), postAndUserDto.getBody());
+        UserException userException = assertThrows(UserException.class,
+                () -> postService.updatePost(dto.getPostId(), postUpdateRequest, dto.getUserName()));
 
-        UserException exception = Assertions.assertThrows(UserException.class, ()
-                -> postService.updatePost(postAndUserDto.getPostId(), postUpdateRequest ,postAndUserDto.getUserName()));
+        //ErrorCode 검사
+        Assertions.assertEquals(userException.getErrorCode(),ErrorCode.INVALID_PERMISSION);
 
-        assertEquals(ErrorCode.INVALID_PERMISSION, exception.getErrorCode());
+
     }
 
-}
+    @Test
+    @DisplayName("수정 실패 : 포스트 존재하지 않음")
+    @WithMockUser
+    void 수정_실패_포스트_존재하지않음() {
+        PostAndUser postAndUser = PostAndUser.getDto();
+        //수정 request
+        PostUpdateRequest postUpdateRequest = new PostUpdateRequest("수정될 제목", "수정될 내용");
+        //해당 post존재하지 않음 Optional.of(mock(Post.class)) <- 이것을 넣으면 에러가 발생하면 안됨
+        when(postRepository.findById(any())).thenReturn(Optional.empty());
+        when(userRepository.findOptionalByUserName(any())).thenReturn(Optional.of(mock(User.class)));
+
+        PostException postException =
+                assertThrows(PostException.class,
+                        () -> postService.updatePost(postAndUser.getPostId(), postUpdateRequest, postAndUser.getUserName()));
+
+        Assertions.assertEquals(postException.getErrorCode(),ErrorCode.POST_NOT_FOUND);
+    }
+
+        @Test
+        @DisplayName("수정 실패 : 회원 존재하지 않음")
+        @WithMockUser
+        void 수정_실패_회원_존재하지않음() {
+            PostAndUser postAndUser = PostAndUser.getDto();
+            //수정 request
+            PostUpdateRequest postUpdateRequest = new PostUpdateRequest("수정될 제목", "수정될 내용");
+
+            //해당 user존재 하지않는 상황가정
+            when(postRepository.findById(any())).thenReturn(Optional.of(mock(Post.class)));
+            when(userRepository.findOptionalByUserName(any())).thenReturn(Optional.empty());
+
+            UserException userException =
+                    assertThrows(UserException.class,
+                            () -> postService.updatePost(postAndUser.getPostId(), postUpdateRequest, postAndUser.getUserName()));
+
+            Assertions.assertEquals(userException.getErrorCode(),ErrorCode.USERNAME_NOT_FOUND);
+        }
+
+
+       /* @Test
+        @WithMockUser
+        @DisplayName("포스트 수정 성공")
+        void postUpdateSuccess() {
+
+            PostAndUser postAndUser = PostAndUser.getDto();
+            PostUpdateRequest postUpdateRequest = new PostUpdateRequest("수정될 제목", "수정될 내용");
+
+            //수정 성공 로직
+            when(userRepository.findOptionalByUserName(any())).thenReturn(Optional.of(mock(User.class)));
+            when(postRepository.findById(any())).thenReturn(Optional.of(mock(Post.class)));
+
+            PostUpdateResponse postUpdateResponse
+                    = postService.updatePost(postAndUser.getPostId(), postUpdateRequest, postAndUser.getUserName());
+
+            Assertions.assertEquals(postUpdateResponse.getMessage(),"포스트 수정 완료");
+
+        }*/
+    }
+    }
