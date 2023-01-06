cff231522a6c81a40918532d065d67d010ca927a
KimGunWoo9595 <medneter@naver.com>
2023-01-06 09:08:51 +0900

    Ref : @PathVaralbe 이름 수정

--- src/main/java/com/example/crudpersional/controller/CommentController.java
+++ src/main/java/com/example/crudpersional/controller/CommentController.java
@@ -28,9 +28,9 @@ public class CommentController {
 
     /**댓글 달기**/
     @ApiOperation(value = "해당 포스트 댓글 달기 ", notes = "postId로 들어온 Post글 댓글 달기 API")
-    @PostMapping("/{id}/comments")
-    public Response<CommentResponse> commentFromPost(@PathVariable Long id, @RequestBody PostCommentRequest postCommentRequest,@ApiIgnore Authentication authentication) {
-        CommentResponse commentResponse = postService.writeComment(id, postCommentRequest.getComment(), authentication.getName());
+    @PostMapping("/{postId}/comments")
+    public Response<CommentResponse> commentFromPost(@PathVariable Long postId, @RequestBody PostCommentRequest postCommentRequest,@ApiIgnore Authentication authentication) {
+        CommentResponse commentResponse = postService.writeComment(postId, postCommentRequest.getComment(), authentication.getName());
         return Response.success(commentResponse);
     }
 
@@ -38,29 +38,29 @@ public class CommentController {
 
     /**댓글 수정**/
     @ApiOperation(value = "해당 포스트 댓글 수정하기 ", notes = "댓글 수정 하기 API")
-    @PutMapping("/{id}/comments")
-    public Response<CommentUpdateResponse> updateComment(@PathVariable Long id, @RequestBody CommentModifyRequest commentModifyRequest, @ApiIgnore Authentication authentication) {
-        CommentUpdateResponse response = postService.modifyComment(id, commentModifyRequest.getComment(), authentication.getName());
+    @PutMapping("/{postId}/comments")
+    public Response<CommentUpdateResponse> updateComment(@PathVariable Long postId, @RequestBody CommentModifyRequest commentModifyRequest, @ApiIgnore Authentication authentication) {
+        CommentUpdateResponse response = postService.modifyComment(postId, commentModifyRequest.getComment(), authentication.getName());
         return Response.success(response);
     }
 
     /**댓글 삭제**/
     @ApiOperation(value = "해당 포스트 댓글 삭제하기 ", notes = "댓글 삭제 하기 API")
-    @DeleteMapping("/{id}/comments")
-    public Response<CommentDeleteResponse> deleteComment(@PathVariable Long id, @ApiIgnore Authentication authentication) {
-        postService.deleteComment(id,authentication.getName());
-        return Response.success(new CommentDeleteResponse("댓글 삭제 완료",id));
+    @DeleteMapping("/{postId}/comments")
+    public Response<CommentDeleteResponse> deleteComment(@PathVariable Long postId, @ApiIgnore Authentication authentication) {
+        postService.deleteComment(postId,authentication.getName());
+        return Response.success(new CommentDeleteResponse("댓글 삭제 완료",postId));
     }
 
     /**해당 포스트 댓글 조회(최신순)**/
     @ApiOperation(value = "해당 포스트 댓글 최신순 조회", notes = "id : 포스트 번호(@PathVariable) , paging : 20개 , 최신순 정렬 API")
-    @GetMapping("/{id}/comments")
-    public Response<Page<CommentResponse>> getComments(@PathVariable Long id,
+    @GetMapping("/{postId}/comments")
+    public Response<Page<CommentResponse>> getComments(@PathVariable Long postId,
                                                        @PageableDefault(size = 10,
                                                                sort = "registeredAt",
                                                                direction = Sort.Direction.DESC) Pageable pageable
     ) {
-        Page<CommentResponse> commentResponses = postService.getComments(id, pageable);
+        Page<CommentResponse> commentResponses = postService.getComments(postId, pageable);
         return Response.success(commentResponses);
     }
 
